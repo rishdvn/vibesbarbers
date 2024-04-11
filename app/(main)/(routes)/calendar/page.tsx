0a1713d628bdc1b674227a710dff5c966792bae8
rishdvn <66307171+rishdvn@users.noreply.github.com>
2024-04-11 16:26:10 +1000

    Update page.tsx

--- app/(main)/(routes)/calendar/page.tsx
+++ app/(main)/(routes)/calendar/page.tsx
@@ -1,8 +1,4 @@
-import CalendarSchedule from "../../_components/calendarschedule";
-import DayView from "../../_components/dayview";
 import MultipleSchedule from "../../_components/multipleSchedule";
-import Sidebar from "../../_components/sidebar";
-
 
 const Calendar = () => {
     return (
